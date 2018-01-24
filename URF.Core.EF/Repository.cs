e796596fd3b4029b0c212b45657b7ae508cf07fc
Anthony Sneed <tony@tonysneed.com>
2018-01-23 19:12:07 -0600

    Add repository.attach, add test for LoadPropertyAsync.

--- URF.Core.EF/Repository.cs
+++ URF.Core.EF/Repository.cs
@@ -37,10 +37,14 @@ namespace URF.Core.EF
             return item != null;
         }
 
-        public virtual async Task<bool> ExistsAsync<TKey>(TKey keyValue, CancellationToken cancellationToken = default) => await ExistsAsync(new object[] {keyValue}, cancellationToken);
+        public virtual async Task<bool> ExistsAsync<TKey>(TKey keyValue, CancellationToken cancellationToken = default)
+            => await ExistsAsync(new object[] {keyValue}, cancellationToken);
 
         public virtual async Task LoadPropertyAsync(TEntity item, Expression<Func<TEntity, object>> property, CancellationToken cancellationToken = default)
             => await Context.Entry(item).Reference(property).LoadAsync(cancellationToken);
+
+        public virtual void Attach(TEntity item)
+            => Set.Attach(item);
 
         public virtual void Insert(TEntity item)
             => Context.Entry(item).State = EntityState.Added;
@@ -56,6 +60,7 @@ namespace URF.Core.EF
             return true;
         }
 
-        public virtual async Task<bool> DeleteAsync<TKey>(TKey keyValue, CancellationToken cancellationToken = default) => await DeleteAsync(new object[] { keyValue }, cancellationToken);
+        public virtual async Task<bool> DeleteAsync<TKey>(TKey keyValue, CancellationToken cancellationToken = default)
+            => await DeleteAsync(new object[] { keyValue }, cancellationToken);
     }
 }
