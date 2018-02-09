63b1ef98ee112003305c4e83cb28acbbffe62a98
Long Le <lelong37@gmail.com>
2018-02-08 23:20:28 -0600

    Removing unused directives in Query.cs.

--- URF.Core.EF/Query.cs
+++ URF.Core.EF/Query.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Runtime.InteropServices.ComTypes;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
