299af0dc5a154d1a0fa608adbb9fadc2aa7ba51b
Anthony Sneed <tony@tonysneed.com>
2018-02-13 06:41:04 -0600

    Add IMergeable to Entity.

--- URF.Core.EF.Trackable/Entity.cs
+++ URF.Core.EF.Trackable/Entity.cs
@@ -1,15 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using TrackableEntities.Common.Core;
 
 namespace URF.Core.EF.Trackable
 {
-    public abstract class Entity : ITrackable
+    public abstract class Entity : ITrackable, IMergeable
     {
         [NotMapped]
         public TrackingState TrackingState { get; set; }
 
         [NotMapped]
         public ICollection<string> ModifiedProperties { get; set; }
+
+        [NotMapped]
+        public Guid EntityIdentifier { get; set; }
     }
-}
+}   
