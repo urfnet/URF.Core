c953961e67f616cdb0a51e0144f1724e7366d84b
Anthony Sneed <anthony.sneed@hilti.com>
2020-04-14 16:15:02 -0500

    Add MongoDocumentRepository

---
+++ URF.Core.Mongo/MongoDocumentRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace URF.Core.Mongo
+{
+    public class MongoDocumentRepository<TEntity> : DocumentRepository<TEntity> where TEntity : class
+    {
+        public MongoDocumentRepository(IMongoCollection<TEntity> collection) : base(collection) { }
+
+        public virtual async Task<TEntity> FindOneAsync(FilterDefinition<TEntity> filter, CancellationToken cancellationToken = default)
+             => await Collection.Find(filter).SingleOrDefaultAsync(cancellationToken);
+
+        public virtual async Task<TEntity> FindOneAndUpdateAsync(Expression<Func<TEntity, bool>> filter, UpdateDefinition<TEntity> update, CancellationToken cancellationToken = default)
+        {
+            await Collection.FindOneAndUpdateAsync(filter, update, null, cancellationToken);
+            return await FindOneAsync(filter, cancellationToken);
+        }
+
+        public virtual async Task<TEntity> FindOneAndUpdateAsync(FilterDefinition<TEntity> filter, UpdateDefinition<TEntity> update, CancellationToken cancellationToken = default)
+        {
+            await Collection.FindOneAndUpdateAsync(filter, update, null, cancellationToken);
+            return await FindOneAsync(filter, cancellationToken);
+        }
+
+        public virtual async Task<int> DeleteOneAsync(FilterDefinition<TEntity> filter, CancellationToken cancellationToken = default)
+        {
+            var result = await Collection.DeleteOneAsync(filter, cancellationToken);
+            return (int)result.DeletedCount;
+        }
+    }
+}
